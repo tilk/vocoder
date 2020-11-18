1bc025a5dc3131fb41838f01b920eb1694f5f81f
Marek Materzok <tilk@tilk.eu>
2020-11-18 12:56:47 +0100

    Select filters from command line

--- vocoder-audio/example/Main.hs
+++ vocoder-audio/example/Main.hs
@@ -4,6 +4,8 @@ module Main where
 import System.Environment
 import Options.Applicative
 import Data.Semigroup ((<>))
+import Data.List.Split
+import Text.Read
 import Vocoder
 import Vocoder.Window
 import Vocoder.Audio
@@ -25,7 +27,8 @@ data Options = Options {
     optFrameSize :: Maybe Length,
     windowSize :: Length,
     hopSize :: HopSize,
-    windowType :: WindowType
+    windowType :: WindowType,
+    filterOpt :: Filter [] (ResourceT IO)
 }
 
 frameSize opts = maybe (windowSize opts) id $ optFrameSize opts
@@ -37,6 +40,30 @@ windowFun HammingWindow = hammingWindow
 windowFun HannWindow = hannWindow
 windowFun BlackmanWindow = blackmanWindow
 windowFun FlatTopWindow = flatTopWindow
+
+auto2 :: (Read a, Read b) => ReadM (a, b)
+auto2 = maybeReader $ f . splitOn ","
+    where
+    f [a,b] = (,) <$> readMaybe a  <*> readMaybe b
+    f _ = Nothing
+
+filterP :: (Functor f, Monad m) => Parser (Filter f m)
+filterP = (lowpassBrickwall <$> option auto
+             ( long "lowpassBrickwall"
+            <> metavar "FREQ"
+            <> help "Low-pass brickwall filter"))
+      <|> (highpassBrickwall <$> option auto
+             ( long "highpassBrickwall"
+            <> metavar "FREQ"
+            <> help "High-pass brickwall filter"))
+      <|> (uncurry bandpassBrickwall <$> option auto2
+             ( long "bandpassBrickwall"
+            <> metavar "FREQ,FREQ"
+            <> help "Band-pass brickwall filter"))
+      <|> (uncurry bandstopBrickwall <$> option auto2
+             ( long "bandstopBrickwall"
+            <> metavar "FREQ,FREQ"
+            <> help "Band-stop brickwall filter"))
 
 options :: Parser Options
 options = Options
@@ -64,6 +91,7 @@ options = Options
        <> value BlackmanWindow
        <> showDefault
        <> help "Type of STFT window")
+    <*> filterP
 
 myFormat = Format {headerFormat = HeaderFormatWav, sampleFormat = SampleFormatPcm16, endianFormat = EndianFile}
 
@@ -72,12 +100,11 @@ main = execParser opts >>= process
     opts = info (options <**> helper)
             ( fullDesc
            <> progDesc "Process audio file"
-           <> header "etaoin shrdlu")
+           <> header "Phase vocoder audio processing")
 
 process :: Options -> IO ()
 process opts = do
     src :: AudioSource _ Double <- sourceSnd $ sourceFile opts
     let params = make_vocoder_params (frameSize opts) (hopSize opts) (windowFor opts)
-    let trans = lowpassBrickwall 500
-    runResourceT $ sinkSnd (destFile opts) myFormat $ processA params trans src
+    runResourceT $ sinkSnd (destFile opts) myFormat $ processA params (filterOpt opts) src
 
