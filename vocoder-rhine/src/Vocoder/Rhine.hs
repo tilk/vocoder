cf692f9c032ff9699fe74cdd75e50a4c8d077be5
Marek Materzok <tilk@tilk.eu>
2020-12-22 15:33:34 +0100

    Modified Rhine API

--- vocoder-rhine/src/Vocoder/Rhine.hs
+++ vocoder-rhine/src/Vocoder/Rhine.hs
@@ -8,9 +8,9 @@ import Vocoder
 volumeFix :: Monad m => VocoderParams -> MSF m STFTFrame STFTFrame
 volumeFix par = arr $ V.map (* volumeCoeff par) *** id
 
-analysis :: Monad m => VocoderParams -> Phase -> MSF m Frame STFTFrame
-analysis par = mealy $ \a s -> swap $ analysisStep (hopSize par) (frameLength par) s $ doFFT par a
+analysis :: Monad m => VocoderParams -> Phase -> MSF m [Frame] [STFTFrame]
+analysis par = mealy $ \a s -> swap $ analysisStage par s a
 
-synthesis :: Monad m => VocoderParams -> Phase -> MSF m STFTFrame Frame
-synthesis par = mealy $ \a s -> doIFFT par *** id $ swap $ synthesisStep (hopSize par) s a
+synthesis :: Monad m => VocoderParams -> Phase -> MSF m [STFTFrame] [Frame]
+synthesis par = mealy $ \a s -> swap $ synthesisStage par s a
 
