63cdbe388b968c5b8489128f865995e0751c4a56
Marek Materzok <tilk@tilk.eu>
2020-12-23 12:40:12 +0100

    Reduce latency for framesOfS.

--- vocoder-rhine/src/Vocoder/Rhine.hs
+++ vocoder-rhine/src/Vocoder/Rhine.hs
@@ -18,16 +18,16 @@ synthesis :: Monad m => VocoderParams -> Phase -> MSF m [STFTFrame] [Frame]
 synthesis par = mealy $ \a s -> swap $ synthesisStage par s a
 
 framesOfS :: Monad m => Length -> HopSize -> MSF m Frame [Frame]
-framesOfS chunkSize hopSize = mealy f DS.empty
+framesOfS chunkSize hopSize = mealy f $ DS.fromList $ map (id &&& return . flip V.replicate 0) [hopSize, hopSize*2 .. chunkSize-1]
     where
     f :: Frame -> DS.Seq (Length, [Frame]) -> ([Frame], DS.Seq (Length, [Frame]))
-    f next q = (fs, q'')
+    f next q = (outs, q'')
         where
         len = V.length next
         newChunks = map ((len -) &&& (return . flip V.drop next)) [0, hopSize .. len-1]
         q' = fmap ((+ len) *** (next :)) q DS.>< DS.fromList newChunks
         (r, q'') = DS.spanl ((>= chunkSize) . fst) q'
-        fs = map (V.take chunkSize . mconcat . reverse . snd) $ toList r
+        outs = map (V.take chunkSize . mconcat . reverse . snd) $ toList r
 
 sumFramesS :: Monad m => Length -> HopSize -> MSF m [Frame] Frame
 sumFramesS chunkSize hopSize = mealy f DS.empty
