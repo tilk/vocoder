c06a32000601d6f4f5f666386a31ca654abb6b57
Marek Materzok <tilk@tilk.eu>
2021-01-05 13:15:05 +0100

    Some optimizations for vocoder-dunai

--- vocoder-dunai/src/Vocoder/Dunai.hs
+++ vocoder-dunai/src/Vocoder/Dunai.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE BangPatterns #-}
 module Vocoder.Dunai where
 
 import Data.MonadicStreamFunction
@@ -6,7 +7,7 @@ import Data.Tuple(swap)
 import Data.Maybe(fromMaybe)
 import Data.Foldable
 import qualified Data.Vector.Storable as V
-import Control.Arrow
+import qualified Data.Vector as VV
 import Vocoder
 
 volumeFix :: Monad m => VocoderParams -> MSF m STFTFrame STFTFrame
@@ -27,17 +28,21 @@ volumeFixS par = arr $ V.map (* volumeCoeff par)
 processS :: Monad m => VocoderParams -> MSF m [STFTFrame] [STFTFrame] -> MSF m Frame Frame
 processS par msf = (framesOfS (inputFrameLength par) (hopSize par) >>> process par msf) &&& arr V.length >>> sumFramesWithLengthS (hopSize par) >>> volumeFixS par
 
+data P a = P {-# UNPACK #-} !Length {-# UNPACK #-} !(V.Vector a)
+
+mapP f g (P n c) = P (f n) (g c)
+
 framesOfS :: forall a m. (V.Storable a, Num a, Monad m) => Length -> HopSize -> MSF m (V.Vector a) [V.Vector a]
-framesOfS chunkSize hopSize = mealy f $ reverse $ map (id &&& return . flip V.replicate 0) [hopSize, hopSize*2 .. chunkSize-1]
+framesOfS chunkSize hopSize = mealy f $ VV.fromList $ map (return . flip V.replicate 0) [hopSize, hopSize*2 .. chunkSize-1]
     where
-    f :: V.Vector a -> [(Length, [V.Vector a])] -> ([V.Vector a], [(Length, [V.Vector a])])
+    f :: V.Vector a -> VV.Vector [V.Vector a] -> ([V.Vector a], VV.Vector [V.Vector a])
     f next q = (outs, q'')
         where
         len = V.length next
-        newChunks = map ((len -) &&& (return . flip V.drop next)) [0, hopSize .. len-1]
-        q' = fmap ((+ len) *** (next :)) q ++ newChunks
-        (r, q'') = span ((>= chunkSize) . fst) q'
-        outs = map (V.take chunkSize . mconcat . reverse . snd) $ toList r
+        newChunks = VV.generate (len `div` hopSize) $ (return . flip V.drop next . (len -) . (* hopSize) . (+1))
+        q' = newChunks VV.++ fmap (next :) q
+        (q'', r) = VV.splitAt ((chunkSize-1) `div` hopSize) q'
+        outs = VV.toList $ VV.reverse $ fmap (V.take chunkSize . V.concat . reverse) r
 
 sumFramesS :: forall a m. (V.Storable a, Num a, Monad m) => Length -> HopSize -> MSF m [V.Vector a] (V.Vector a)
 sumFramesS chunkSize hopSize = arr (id &&& const chunkSize) >>> sumFramesWithLengthS hopSize
@@ -45,11 +50,11 @@ sumFramesS chunkSize hopSize = arr (id &&& const chunkSize) >>> sumFramesWithLen
 sumFramesWithLengthS :: forall a m. (V.Storable a, Num a, Monad m) => HopSize -> MSF m ([V.Vector a], Length) (V.Vector a)
 sumFramesWithLengthS hopSize = mealy f []
     where
-    f :: ([V.Vector a], Length) -> [(Length, V.Vector a)] -> (V.Vector a, [(Length, V.Vector a)])
-    f (nexts, chunkSize) q = (next, q'')
+    f :: ([V.Vector a], Length) -> [P a] -> (V.Vector a, [P a])
+    f (nexts, chunkSize) q = (nextv, q'')
         where
-        ith i (n, c0) = fromMaybe 0 $ c0 V.!? (i - n)
-        q' = q ++ zip [0, hopSize..] nexts
-        next = V.generate chunkSize $ \i -> sum $ fmap (ith i) q'
-        q'' = fmap ((+ (-chunkSize)) *** id) $ dropWhile (\(n, c) -> V.length c + n <= chunkSize) q'
+        ith i (P n c0) = fromMaybe 0 $ c0 V.!? (i - n)
+        q' = q ++ zipWith P [0, hopSize..] nexts
+        nextv = V.generate chunkSize $ \i -> sum $ fmap (ith i) q'
+        q'' = map (mapP (+ (-chunkSize)) id) $ dropWhile (\(P n c) -> V.length c + n <= chunkSize) q'
 
